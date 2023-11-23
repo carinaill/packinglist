40b54260fff5c129aabaed1c94cefd1f2dadbabf
Carina <cillgen@cgs-online.de>
2023-11-23 14:52:10 +0100

    database connection

--- src/main/java/de/cillgen/packinglist/Overview.java
+++ src/main/java/de/cillgen/packinglist/Overview.java
@@ -1,50 +1,28 @@
 package de.cillgen.packinglist;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.Serializable;
 import java.util.List;
 
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
-@RequestScoped
+@ViewScoped
 @Named
-public class Overview {
-@Inject 
-private Service service;
+public class Overview implements Serializable {
+	@Inject
+	private Service service;
 
 	private List<Card> destinations;
 
 	public void init() {
-		System.out.println(service.testCount());
-		
-		this.destinations = new ArrayList<>();
-		
-		Card spain = new Card("Spanien");
-		spain.setItems(Arrays.asList("flipflops", "sonnencreme", "sonnencreme", "sonnencreme"));
-		this.destinations.add(spain);
-		
-		Card norway = new Card("Norwegen");
-		norway.setItems(Arrays.asList("handschuhe", "mütze", "sonnencreme"));
-		this.destinations.add(norway);
-		
-		Card costaRica = new Card("Costa Rica");
-		costaRica.setItems(Arrays.asList("sonnenbrille", "cappie"));
-		this.destinations.add(costaRica);
-		
-		Card spain1 = new Card("Spanien1");
-		spain1.setItems(Arrays.asList("flipflops", "sonnencreme"));
-		this.destinations.add(spain1);
-		Card spain2 = new Card("Spanien2");
-		spain2.setItems(Arrays.asList("flipflops", "sonnencreme", "sonnencreme", "sonnencreme"));
-		this.destinations.add(spain2);
-		Card spain3 = new Card("Spanien3");
-		spain3.setItems(Arrays.asList("flipflops", "sonnencreme"));
-		this.destinations.add(spain3);
-		Card spain4 = new Card("Spanien4");
-		spain4.setItems(Arrays.asList("flipflops", "sonnencreme", "sonnencreme"));
-		this.destinations.add(spain4);
+		// service.testInit();
+		destinations = service.testLoad();
+	}
+
+	public void onClick(String item) {
+		System.out.println(item);
+
 	}
 
 	public List<Card> getDestinations() {
--- src/main/java/de/cillgen/packinglist/Service.java
+++ src/main/java/de/cillgen/packinglist/Service.java
@@ -1,17 +1,61 @@
 package de.cillgen.packinglist;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 
 @ApplicationScoped
 public class Service {
 
+	@Inject
+	private EntityManager em;
+
 	public int testCount() {
-		EntityManager em = Persistence.createEntityManagerFactory("default").createEntityManager();
 		Query query = em.createNativeQuery("SELECT COUNT(*) FROM DESTINATION");
 		return ((Number) query.getSingleResult()).intValue();
 	}
-	
+
+	public void testInit() {
+		Destination destination = new Destination();
+		destination.setName("Spanien");
+
+		EntityTransaction entityTransaction = em.getTransaction();
+		try {
+			entityTransaction.begin();
+			em.persist(destination);
+			entityTransaction.commit();
+		} catch (Exception e) {
+			entityTransaction.rollback();
+		}
+	}
+
+	public List<Card> testLoad() {
+		List<Destination> destinations = em.createQuery("SELECT d FROM Destination d", Destination.class)
+				.getResultList();
+		Map<Long, List<String>> itemNamesByDestinationId = em.createQuery("SELECT i FROM Item i", Item.class)
+				.getResultStream()
+				.collect(Collectors.groupingBy(Item::getDestinationId,
+						Collectors.mapping(Item::getName, Collectors.toList())));
+
+		return destinations
+				.stream()
+				.map(dest -> toCard(dest, itemNamesByDestinationId))
+				.collect(Collectors.toList());
+	}
+
+	private Card toCard(Destination dest, Map<Long, List<String>> itemNamesByDestinationId) {
+		Card card = new Card(dest.getName());
+		List<String> itemNames = itemNamesByDestinationId.getOrDefault(dest.getId(),
+				Collections.emptyList());
+		card.setItems(itemNames);
+		return card;
+	}
+
 }
